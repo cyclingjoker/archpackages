248fbf7eb71d235d0d5618c687fdc4a033ed7afe
cyclingjoker <70005671+cyclingjoker@users.noreply.github.com>
2020-09-01 14:47:47 -0400

    Add files via upload

---
+++ baseinstall.sh
@@ -0,0 +1,9 @@
+sudo pacman -S xorg-server xorg-apps xorg-xinit i3 lightdm lightdm-gtk-greeter alsa-utils alsa-plugins alsa-lib pavucontrol ranger dmenu kitty firefox --noconfirm --needed
+
+
+
+
+
+
+sudo systemctl enable lightdm
+sudo systemctl start lightdm
